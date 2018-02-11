f8bb5d7bb1eec17bbf0a6af56345cad5a50de766
electriwizard <e1ectricwiz4rd@gmail.com>
2018-02-12 00:17:03 +0300

    added wordmix main with console interface

---
+++ wordmix_main.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# C:/Python27/python.exe
+from moviepy.editor import *
+from moviepy.video.fx.all import *
+import random
+import time
+
+from generate_sequence import *
+from files_scanner import *
+import sys
+
+class VideoEditor():
+	pass
+
+def test():
+	pass
+
+
+# =======================================================================OUT====================================
+
+def randclip(maxclips):
+	return random.randint(0, maxclips)
+
+def cut_logic(exec_numb):
+	clips = []
+	print(clips)
+	clipsList = files_scanner_video(path)
+	clipsCounter = len(clipsList) - 1
+	for i, objects in enumerate(clipsList[::1]):
+		for j in range(0, 4):
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 3)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(loop(generate_rand_sequence(clipsList[randclip(clipsCounter)], 1, 0.2), 3))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 3)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 3)))
+			clips.append(generate_rand_sequence(clipsList[randclip(clipsCounter)], 0, random.uniform(1, 4)))
+
+		if i % random.randint(2, 3) == 0:
+			clips[i] = time_symmetrize(clips[i])
+
+	concat_and_write(clips, exec_numb)
+
+def resulst_store(clips, exec_numb):
+	pass
+
+def concat_and_write(clips, exec_numb):
+	write_data = time.strftime("%I%M%S")
+	print(write_data)
+	try:
+		clipOut = concatenate_videoclips(clips, method='compose')
+		clipOut.write_videofile("./" + write_data + "-out.mp4",fps=25)
+	except Exception as e:
+		print(str(e))
+		print("An error occured, try {} times".format(exec_numb))
+		if exec_numb > 0:
+			exec_numb -= 1
+			cut_logic(exec_numb)
+		else:
+			print("game over")
+
+if __name__ == "__main__":
+	try:
+		path = sys.argv[1]
+		print(files_scanner_video(path))
+		exec_numb = 5
+		dur = []
+		cut_logic(exec_numb)
+	except IndexError as e:
+		# print(str(e))
+		print("please enter the path")
