67b543dd88559d0f080657274235890ce64689cf
electriwizard <e1ectricwiz4rd@gmail.com>
2018-03-11 15:12:37 +0300

    added envoder, fixed Sony vegas format incompatibility

--- output_video/encoding-to-vegas.sh
+++
@@ -1,6 +0,0 @@
-DIRNAME="./encoded/"
-mkdir $DIRNAME
-for i in $(ls ./ | egrep "mp4|avi");
-do 
- ffmpeg -i $i -acodec aac -vcodec libx264 -strict experimental -tune fastdecode $DIRNAME+$i+"_vegas-enc.mp4"
-done
--- videocringe.py
+++ videocringe.py
@@ -26,7 +26,8 @@ class Videocringe():
         for i in self.clips:
             for j in range(0,times):
                 self.sequences.append(Generator.rand_sequence(i))
-        print(self.sequences)
+        random.shuffle(self.sequences)
+        # print(self.sequences)
 
     '''helper function for getting filename (or dirname) from path'''
     def get_filename(self):
@@ -37,9 +38,9 @@ class Videocringe():
         for i, sequence in enumerate(self.sequences):
             self.sequences[i] = sequence.resize( (1920, 1080) )
         clipOut = concatenate_videoclips(self.sequences, method='compose')
-        clipOut.write_videofile("./output_video/" + self.get_filename() + self.date + "-out.mp4", fps=30)
+        clipOut.write_videofile("./output_video/" + self.get_filename() + self.date + "-out.mp4", fps=30, codec='libx264', audio_codec='aac')
 
 if __name__ == '__main__':
     editor = Videocringe(sys.argv[1])
-    editor.cut_video(0.1,1,10)
+    editor.cut_video(1,5,10)
     editor.write_video()
