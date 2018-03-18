6848041bcd31531b13ebead8bb94bc67db0789c1
electriwizard <e1ectricwiz4rd@gmail.com>
2018-03-19 01:14:56 +0300

    merging shiet

--- encoding-to-vegas.sh
+++ encoding-to-vegas.sh
@@ -6,3 +6,5 @@ for i in $(ls $1 | egrep "mp4|avi");
 do 
  ffmpeg -i $1/$i -acodec aac -vcodec libx264 -strict experimental -tune fastdecode $OUTDIRNAME+$i+"_vegas-enc.mp4"
 done
+
+# Making an octopus merge
